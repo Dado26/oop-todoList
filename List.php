6bfd8073b215405ddffef088b5192fc7ed7b5e56
Daniel Cerovsky <danielcerovsky@yahoo.com>
2018-09-09 16:30:18 +0200

    Initial commit

---
+++ List.php
@@ -0,0 +1,32 @@
+<?php
+
+class ToDoList {
+    private $tasks;
+    
+    public function getTasks(){
+        return $this->tasks;
+    }
+    
+    public function addTask(Task $task) {
+        $this->tasks[]= $task;
+    }
+    
+    public function removeTask($index) {
+        array_splice($this->tasks, $index, 1);
+    }
+    
+    public function clearFinishedTasks() {
+        foreach($this->tasks as $index => $task) {
+            if( $task->isFinished() ){
+                $this->removeTask($index);
+            }
+        }
+    }
+    
+    public function finishTask($index) {
+        $this->tasks[$index]->finish();
+    }
+    public function unfinishTask($index){
+        $this->tasks[$index]->unfinish();
+    }
+}
